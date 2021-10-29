8987273cefe4d482e3c8090c267425e86213b06e
Sui <suiyutong95@qq.com>
2021-10-29 18:00:30 +0800

    disabled get_top_infos

--- utils/system.py
+++ utils/system.py
@@ -5,39 +5,42 @@ import datetime
 import psutil
 import json
 
+
 def get_top_info():
+    # lines=os.popen('top -bn 1 -p {}'.format(os.getpid())).read().split('\n')
+    # total_load=lines[0].split('load average:')[-1].replace(',','')[1:]
+    # try:
+    #     total_load=[float(x) for x in total_load[0:1]]
+    # except:
+    #     total_load=0
+    # cpu_rate=float([a for a in lines[-2].split('') if a!=''][8])
+    # mem_rate=float([a for a in lines[-2].split('') if a!=''][9])
+    # try:
+    #     dur_time=([a for a in lines[-2].split('')if a!=''][10])
+    # except:
+    #     dur_time=0
+    # return total_load,cpu_rate,mem_rate,dur_time
+    return 0, 0, 0, 0
 
-    lines=os.popen('top -bn 1 -p {}'.format(os.getpid())).read().split('\n')
-    total_load=lines[0].split('load average:')[-1].replace(',','')[1:]
-    try:
-        total_load=[float(x) for x in total_load[0:1]]
-    except:
-        total_load=0
-    cpu_rate=float([a for a in lines[-2].split('') if a!=''][8])
-    mem_rate=float([a for a in lines[-2].split('') if a!=''][9])
-    try:
-        dur_time=([a for a in lines[-2].split('')if a!=''][10])
-    except:
-        dur_time=0
-    return total_load,cpu_rate,mem_rate,dur_time
 
-def list_and_save_all_member(cls,save_path=None):
-
-    cfg={}
-    for name,value in vars(cls).items():
-        print('    - self.{}:{}'.format(name,value))
-        cfg[name]=str(value)
+def list_and_save_all_member(cls, save_path=None):
+    cfg = {}
+    for name, value in vars(cls).items():
+        print('    - self.{}:{}'.format(name, value))
+        cfg[name] = str(value)
     if save_path is None:
         return None
-    with open(save_path,'w') as f:
-        json.dump(cfg,f)
+    with open(save_path, 'w') as f:
+        json.dump(cfg, f)
 
-def force_print(*args,**kwargs):
-    print(*args,**kwargs)
+
+def force_print(*args, **kwargs):
+    print(*args, **kwargs)
     sys.stdout.flush()
 
+
 def get_cpu_nums():
-    optimal_cpu_count=max(max(
+    optimal_cpu_count = max(max(
         psutil.cpu_count(logical=False),
-        int(os.getenv('BIOMIND_SERVER_CPUS',1)))-1,1)
+        int(os.getenv('BIOMIND_SERVER_CPUS', 1)))-1, 1)
     return optimal_cpu_count
